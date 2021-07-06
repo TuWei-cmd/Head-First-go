3026a53224adccafe1499337d84f205c16d27c81
Tuwei <72648369+TuWei-cmd@users.noreply.github.com>
2021-07-06 19:51:59 +0800

    Update pass_fail.go

--- 2-ConditionalsAndLoops/p34-pass_fail/pass_fail/pass_fail.go
+++ 2-ConditionalsAndLoops/p34-pass_fail/pass_fail/pass_fail.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -15,12 +17,13 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(input)
-	// if int(input) == 100 {
-	// 	fmt.Println("Perfect")
-	// } else if int(input) >= 60 {
-	// 	fmt.Println("You pass.")
-	// } else {
-	// 	fmt.Println("You fail!")
-	// }
+	input = strings.TrimSpace(input)
+	grade, err := strconv.ParseFloat(input, 64)
+	if grade == 100.0 {
+		fmt.Println("Perfect")
+	} else if grade >= 60.0 {
+		fmt.Println("You pass.")
+	} else {
+		fmt.Println("You fail!")
+	}
 }
