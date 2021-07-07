31fb4e43a05c72e386338c7778962740f5f4e697
Tuwei <72648369+TuWei-cmd@users.noreply.github.com>
2021-07-08 01:09:38 +0800

    Update paintNeeded.go

--- 3/p88-paintNeeded/paintNeeded.go
+++ 3/p88-paintNeeded/paintNeeded.go
@@ -2,20 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
-func paintNeeded(width float64, height float64) float64 {
+func paintNeeded(width float64, height float64) (float64, error) {
+	if width < 0 {
+		return 0, fmt.Errorf("a width of %0.2f is invalid", width)
+	}
+	if height < 0 {
+		return 0, fmt.Errorf("a height of %0.2f is invalid", height)
+	}
 	area := width * height
-	return area / 10.0
+	return area / 10.0, nil
 }
 
 func main() {
-	var amount, total float64
-	amount = paintNeeded(4.2, 3.0)
+	amount, err := paintNeeded(4.2, -3.0)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Printf("%.2f liters needed\n", amount)
-	total += amount
-	amount = paintNeeded(1.2, 9.0)
-	fmt.Printf("%.2f liters needed\n", amount)
-	total += amount
-	fmt.Printf("Total: %.2f liters\n", total)
 }
