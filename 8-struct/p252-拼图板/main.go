388e933b27aae4e1c2a0c2884d2218803397c68e
Tuwei <72648369+TuWei-cmd@users.noreply.github.com>
2021-07-23 15:57:58 +0800

    Update main.go

--- 8-struct/p252-拼图板/main.go
+++ 8-struct/p252-拼图板/main.go
@@ -10,6 +10,8 @@ func main() {
 		Latitude:  37.42,
 		Longitude: -122.08,
 	}
+	location.SetLatitude(38.2)
+	location.SetLongitude(-123.4)
 	fmt.Println("Latitude:", location.Latitude)
 	fmt.Println("Longitude:", location.Longitude)
 }
